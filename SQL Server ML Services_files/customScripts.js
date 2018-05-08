e2974953446797bb15567d6708378187195061f1
Aniruddh Munde <anmunde@microsoft.com>
2018-05-07 18:38:26 -0700

    use jupyter hub
    On load, updateUrl with random user.
    On click, do POST and GET calls.

---
+++ SQL Server ML Services_files/customScripts.js
@@ -0,0 +1,57 @@
+var login_url = "http://52.229.58.170:8000/hub/login"
+
+function updateUrl() {
+    var randomNumber = Math.floor(Math.random() * 100);
+    element = document.getElementById("notebook1")
+    username = "user" + randomNumber;
+    password = "user" + randomNumber + "magickey"
+
+    if (element != null) {
+        setCredentials(username, password);
+        element.setAttribute("href","http://52.229.58.170:8000/user/user" + randomNumber + "/notebooks/user" + randomNumber);
+    }
+};
+
+function goToJupyterHub(id) {
+    element = document.getElementById(id);
+    url = element.getAttribute("href");
+    username = element.getAttribute("username");
+    password = element.getAttribute("password");
+    ret = $.ajax(login_url,
+        {
+            method: 'POST',
+            data: { 'username': username, 'password': password },
+            crossDomain: true,
+        });
+    $.ajax(url,
+        {
+            method: 'GET',
+            xhrFields: { withCredentials: true },
+            crossDomain: true
+        });
+}
+
+function setCredentials(username, password) {
+    element = document.getElementById("notebook1")
+    if (element != null) {
+        element.setAttribute("username", username);
+        element.setAttribute("password", password);
+    }
+}
+
+function showCredentials() {
+    element = document.getElementById("notebook1");
+    username = element.getAttribute("username");
+    password = element.getAttribute("password");
+    var creds = window.open('', 'Credentials', 'width=400,height=100');
+    creds.document.write(`<html>
+        <head>
+        <title>Credentials information</title>
+        </head>
+        <body>
+            <p> Please use the following credentials : </p>
+            <p>Username = ` + username + `
+           <br>Password = ` + password + `</p >
+        </body>
+        </html>`);
+}
